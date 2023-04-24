21e905bea9ba945d61b5cfd89e45a78815f839dd
CrispyPin <crispin@tasa.se>
2023-04-24 21:18:04 +0200

    update autostart

--- awesome/modules/autostart.lua
+++ awesome/modules/autostart.lua
@@ -7,24 +7,23 @@ if is_reloading() then
 	return
 end
 
-awful.spawn(CONFIG_DIR .. "utils/xcape_conf.sh") -- xcape config
-awful.spawn("picom -b") -- compositor
+awful.spawn(CONFIG_DIR .. "utils/xcape_conf.sh")
+awful.spawn("picom -b")
 awful.spawn("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1") -- authentication popup for obs virtual camera etc
 awful.spawn("redshift")
 awful.spawn("sxhkd")
 
 if not is_laptop then
-	awful.spawn("numlockx on") -- numlock
-	awful.spawn(CONFIG_DIR .. "utils/xrandr_conf.sh") -- xrandr config
+	awful.spawn(CONFIG_DIR .. "utils/xrandr_conf.sh")
 
 	awful.spawn("discord", { screen = 3 })
 	awful.spawn("vscodium")
 	awful.spawn("spotify", { tag = "2", screen = 3, urgent = false })
 	awful.spawn("firefox")
 
-	awful.spawn(terminal .. " -e fish -c \"sleep 2 && gomuks\"", { tag = "3", screen = 3, urgent = false, focus = false })
 	awful.spawn(terminal .. " -e fish -c \"sleep 5 && ncpamixer\"",
-		{ tag = "4", screen = 3, urgent = false, focus = false })
-	awful.spawn(terminal .. " -e btop", { tag = "5", screen = 3, urgent = false, focus = false })
-	awful.spawn(terminal .. " -e fish -c \"sleep 5 && snoud\"", { tag = "6", screen = 3, urgent = false, focus = false })
+		{ tag = "3", screen = 3, urgent = false, focus = false })
+	awful.spawn(terminal .. " -e btop", { tag = "4", screen = 3, urgent = false, focus = false })
+	awful.spawn(terminal .. " -e fish -c \"sleep 5 && snoud\"", { tag = "5", screen = 3, urgent = false, focus = false })
+	awful.spawn(terminal .. " -e fish -c \"sleep 2 && gomuks\"", { tag = "6", screen = 3, urgent = false, focus = false })
 end
