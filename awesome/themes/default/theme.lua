3b44a4bf6f5aadf96d30dd5540641214e379d09c
CrispyPin <crispin@tasa.se>
2025-03-03 22:23:19 +0100

    a

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -102,7 +102,7 @@ theme.menu_border_color         = "#ff8844"
 -- theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 -- theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper                 = theme_path .. "background.png"
+theme.wallpaper                 = theme_path .. "background.jpg"
 
 -- You can use your own layout icons like this:
 -- theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
---
+++ steam/launch-options/space-engineers-launch-options.md
@@ -0,0 +1,3 @@
+LD_PRELOAD="" %command% -nosplash -skipintro -useallavailablecores
+
+LD_PRELOAD="" fixes 55 min lag bug
